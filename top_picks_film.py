eab032d2afb97a2d16167c39fae7ab433052ecfc
Ida Ernawati <125109186+idaaernawati@users.noreply.github.com>
2024-06-20 23:12:26 +0700

    Update top_picks_film.py

--- top_picks_film.py
+++ top_picks_film.py
@@ -26,7 +26,7 @@ selected_aspect = st.sidebar.selectbox(
 if selected_aspect == "Comparison":
     st.subheader("Comparison Chart - Bar Chart")
     # Contoh Bar Chart: Membandingkan rating film
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(11, 7))
     sns.barplot(x='Title', y='Rating', data=df1)
     plt.xticks(rotation=90)
     plt.title("Comparison of Movie Ratings")
@@ -38,7 +38,7 @@ if selected_aspect == "Comparison":
 elif selected_aspect == "Relationship":
     st.subheader("Relationship Chart - Scatter Plot")
     # Contoh Scatter Plot: Hubungan antara budget dan gross
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(12, 8))
     sns.scatterplot(x='Budget', y='Gross_us', data=df1)
     plt.title("Relationship between Budget and Gross Revenue")
     st.pyplot(plt)
@@ -50,7 +50,7 @@ elif selected_aspect == "Composition":
     st.subheader("Composition Chart - Donut Chart")
     # Contoh Donut Chart: Komposisi genre film
     genre_counts = df1['Genre'].value_counts()
-    plt.figure(figsize=(15, 17))
+    plt.figure(figsize=(17, 19))
     plt.pie(genre_counts, labels=genre_counts.index, autopct='%1.1f%%', startangle=140, wedgeprops=dict(width=0.3))
     plt.title("Composition of Movie Genres")
     st.pyplot(plt)
@@ -61,7 +61,7 @@ elif selected_aspect == "Composition":
 elif selected_aspect == "Distribution":
     st.subheader("Distribution Chart - Line Chart")
     # Contoh Line Chart: Distribusi rating film
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(13, 9))
     sns.lineplot(data=df1['Rating'])
     plt.title("Distribution of Movie Ratings")
     st.pyplot(plt)
